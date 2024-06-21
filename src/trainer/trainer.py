36dee325b888c48092e8cebb440e34f6c02c18a9
Jun-Min Lee <ljm56897@gmail.com>
2024-06-21 17:51:18 +0900

    fix: training mode 아닐 시 발생하는 데이터 로드 버그 수정

--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -57,7 +57,7 @@ class Trainer:
             SCHEDULER_MSG + f' but got {colorstr(self.scheduler_type)}'
 
         # init model, dataset, dataloader, etc.
-        self.modes = ['train', 'validation'] if self.is_training_mode else ['test']
+        self.modes = ['train', 'validation'] if self.is_training_mode else ['train', 'validation', 'test']
         self.model = self._init_model(self.config, self.mode)
         self.dataloaders = get_data_loader(self.config, self.modes, self.is_ddp)
         self.training_logger = TrainingLogger(self.config, self.is_training_mode)
